6c8a84728d4a3ee44828f352270d36ad1d9493ca
Satyam Pratap Singh <39654260+singhsatyampratap@users.noreply.github.com>
2019-12-27 12:34:06 +0530

    Update spectral_analysis.py
    saving .png image of amplitude spectrum

--- ANT_SPS/spectral_analysis.py
+++ ANT_SPS/spectral_analysis.py
@@ -1,21 +1,37 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Mon Dec 16 14:31:04 2019
+Created on Tue Dec 24 00:08:56 2019
 
 @author: singhsatyampratap
 """
-
-""" This portion of the code is useful to perform spectral analysis and spectrogram """
-
 import obspy
 import numpy as np
-def spectral_analysis(tr):
-    nsamp = tr.stats.sampling_rate                              # sampling rate
-    fNy = nsamp / 2.0                          # Nyquist frequency
-    y=tr.data
-    y_f = np.fft.rfft(y)                    # transform all 3 signals into frequency domain 
-    freq = np.linspace(0, fNy, len(y_f))    # frequency axis for plotting
-    return freq, y_f
+import matplotlib.pyplot as plt  
+#amplitude spectrum plot
+def spectrum_show(st,fig_name):
+    st.taper(0.01)
+    st.resample(1.0)
+    dat=st[0].data
+    samp = len(dat)                                # number of sample point
+    delta = st[0].stats.delta                             
+   
 
-
+  
+    # FFT data into frequency-domain
+    Fdat = np.fft.rfft(dat, n=samp)
+    
+    # x-axis in f-domain for plotting
+    xf = np.linspace(0.0, 1.0/(2.0*delta), (samp/2)+1)
+    
+    # plot
+    
+    plt.title('Frequency Domain')
+    plt.plot(xf, 2.0/samp * np.abs(Fdat), color='b',label="no Taper",linewidth=1.5)
+    plt.xlabel('Frequency [Hz]')
+    plt.ylabel('Amplitude')
+    plt.ylim([0,20])
+    plt.figure(figsize=(20,5))
+    plt.savefig('.png')
+    plt.show()
+    return xf, Fdat   
